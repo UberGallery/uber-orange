ed61165214c54864fa646ff2de82bc093b311045
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2013-07-23 13:08:30 -0700

    Updated remote jQuery call to v2.0.3

--- index.php
+++ index.php
@@ -4,23 +4,23 @@
 <head>
     <title>UberGallery</title>
     <link rel="shortcut icon" href="<?php echo THEMEPATH; ?>/images/favicona.png" />
-    
+
     <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/rebase-min.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo THEMEPATH; ?>/style.css" />
     <?php echo $gallery->getColorboxStyles(1); ?>
-    
-    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <?php echo $gallery->getColorboxScripts(); ?>
-    
+
     <?php file_exists('googleAnalytics.inc') ? include('googleAnalytics.inc') : false; ?>
-    
+
 </head>
 <body>
 
 <!-- Start UberGallery v<?php echo UberGallery::VERSION; ?> - Copyright (c) <?php echo date('Y'); ?> Chris Kankiewicz (http://www.ChrisKankiewicz.com) -->
 <div id="galleryWrapper">
     <h1>UberGallery</h1>
-    
+
     <?php if($gallery->getSystemMessages()): ?>
         <ul id="systemMessages">
             <?php foreach($gallery->getSystemMessages() as $message): ?>
@@ -30,7 +30,7 @@
             <?php endforeach; ?>
         </ul>
     <?php endif; ?>
-    
+
     <div id="galleryListWrapper">
         <?php if (!empty($galleryArray) && $galleryArray['stats']['total_images'] > 0): ?>
             <ul id="galleryList" class="clearfix">
@@ -40,25 +40,25 @@
             </ul>
         <?php endif; ?>
     </div>
-    
+
     <div id="galleryFooter" class="clearfix">
-    
+
         <?php if ($galleryArray['stats']['total_pages'] > 1): ?>
         <ul id="galleryPagination">
-            
+
             <?php foreach ($galleryArray['paginator'] as $item): ?>
-                
+
                 <li class="<?php echo $item['class']; ?>">
                     <?php if (!empty($item['href'])): ?>
                         <a href="<?php echo $item['href']; ?>"><?php echo $item['text']; ?></a>
                     <?php else: ?><?php echo $item['text']; ?><?php endif; ?>
                 </li>
-            
+
             <?php endforeach; ?>
-            
+
         </ul>
         <?php endif; ?>
-        
+
         <div id="credit">Powered by, <a href="http://www.ubergallery.net">UberGallery</a></div>
     </div>
 </div>
